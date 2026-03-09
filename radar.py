7b4f875d47151c57483df42c3416f21f04383340
sanbr06 <s.aandylima@gmail.com>
2026-03-09 15:23:32 -0300

    Update radar.py

--- radar.py
+++ radar.py
@@ -3,8 +3,7 @@ import requests
 TOKEN="SEU_TOKEN"
 CHAT_ID="5965060661"
 
-url=f"https://api.telegram.org/bot8735265703:AAF-iaDIDuDLujTSzAuShHSSdcXtTr7HDag
-/sendMessage"
+url=f"https://api.telegram.org/bot8735265703:AAF-iaDIDuDLujTSzAuShHSSdcXtTr7HDag/sendMessage"
 
 requests.post(url,data={
     "chat_id":CHAT_ID,
