7c8ac422b8d93bb7d30b9df224ee1e65e2367102
sanbr06 <s.aandylima@gmail.com>
2026-03-09 15:32:58 -0300

    Update radar.py

--- radar.py
+++ radar.py
@@ -1,4 +1,5 @@
 import requests
+import re
 
 TOKEN="SEU_TOKEN"
 CHAT_ID="5965060661"
@@ -14,42 +15,55 @@ def alerta(msg):
 
 
 headers={
-    "User-Agent":"Mozilla/5.0"
+"User-Agent":"Mozilla/5.0"
 }
+
 
 produtos={
 
 "Ryzen 7600":[
-
 "https://www.kabum.com.br/busca/ryzen-7600",
 "https://www.pichau.com.br/search?q=ryzen%207600",
-"https://www.terabyteshop.com.br/busca?str=ryzen+7600",
-"https://www.amazon.com.br/s?k=ryzen+7600",
-"https://lista.mercadolivre.com.br/ryzen-7600"
-
+"https://www.terabyteshop.com.br/busca?str=ryzen+7600"
 ],
 
 "RX 7600":[
-
 "https://www.kabum.com.br/busca/rx-7600",
 "https://www.pichau.com.br/search?q=rx%207600",
-"https://www.terabyteshop.com.br/busca?str=rx+7600",
-"https://www.amazon.com.br/s?k=rx+7600",
-"https://lista.mercadolivre.com.br/rx-7600"
-
+"https://www.terabyteshop.com.br/busca?str=rx+7600"
 ],
 
 "DDR5 2x8GB":[
-
 "https://www.kabum.com.br/busca/ddr5-16gb-2x8",
 "https://www.pichau.com.br/search?q=ddr5%202x8",
-"https://www.terabyteshop.com.br/busca?str=ddr5+2x8",
-"https://www.amazon.com.br/s?k=ddr5+2x8+16gb",
-"https://lista.mercadolivre.com.br/ddr5-16gb-2x8"
-
+"https://www.terabyteshop.com.br/busca?str=ddr5+2x8"
 ]
 
 }
+
+
+
+def extrair_preco(html):
+
+    precos=re.findall(r"R\$ ?[0-9\.,]+",html)
+
+    valores=[]
+
+    for p in precos:
+
+        v=p.replace("R$","").replace(".","").replace(",",".").strip()
+
+        try:
+            valores.append(float(v))
+        except:
+            pass
+
+    if valores:
+        return min(valores)
+
+    return None
+
+
 
 for produto,urls in produtos.items():
 
@@ -59,10 +73,22 @@ for produto,urls in produtos.items():
 
             r=requests.get(url,headers=headers,timeout=10)
 
-            if r.status_code==200:
+            html=r.text
 
-                alerta(f"🔎 Monitorando {produto}\n{url}")
+            preco=extrair_preco(html)
+
+            if preco:
+
+                alerta(
+f"""🔎 Oferta encontrada
+
+Produto: {produto}
+
+Preço: R${preco}
+
+Loja:
+{url}"""
+)
 
         except:
-
             pass
