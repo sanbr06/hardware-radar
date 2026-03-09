1e56bfea52d558005b95b7b597d958bb038cd729
sanbr06 <s.aandylima@gmail.com>
2026-03-09 15:16:24 -0300

    Update radar.py

--- radar.py
+++ radar.py
@@ -3,11 +3,9 @@ import requests
 TOKEN="SEU_TOKEN"
 CHAT_ID="5965060661"
 
-def alerta(msg):
-    url=f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    requests.post(url,data={"chat_id":CHAT_ID,"text":msg})
+url=f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
-r=requests.get("https://www.kabum.com.br/busca/ryzen-7600")
-
-if "ryzen 7600" in r.text.lower():
-    alerta("Radar monitorando Ryzen 7600")
+requests.post(url,data={
+    "chat_id":CHAT_ID,
+    "text":"🚀 Radar funcionando"
+})
